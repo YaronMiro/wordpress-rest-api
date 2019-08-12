760270329d8bc77f5dd6fc8e0f1f3a905cb83ceb
yaronm <yaron.miro@gmail.com>
2019-08-12 16:44:35 +0300

    Add rest-api-example plugin

---
+++ wp-content/plugins/rest-api-example/rest-api-example.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Rest Api Example
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     rest-api-example
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Rest_Api_Example
+ */
+
+// Your code starts here.
