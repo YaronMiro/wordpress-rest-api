2bd0da36bbcd3072437d24fa9dfc565ae805a5f8
yaronm <yaron.miro@gmail.com>
2019-08-13 13:01:22 +0300

    add dummy code

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -23,8 +23,31 @@ function twentyseventeen_child_enqueue_styles() {
 function twentyseventeen_child_enqueue_scripts() {
     wp_enqueue_script( 'twentyseventeen_child-script', get_stylesheet_directory_uri() . '/js/script.js', array( 'jquery' ), '1.0.0', true );
     
+
+    // $total_movie_posts = wp_count_posts('movie');
+    $query_args = array(
+        'post_type' => 'movie',
+        'post_status' => array(
+            'publish',
+            'pending',
+            'draft',
+            'auto-draft',
+            'future',
+            'private',
+            'inherit',
+            'trash'
+        )    
+    );
+    $movie_posts = new WP_Query($query_args);
+    $total_movie_posts = $movie_posts->found_posts;
+    $posts_per_page = 20;
+
+
     $localize_data = array(
         'SITE_URL' => get_option('siteurl'),
+        'TOTAL_POSTS' => $total_movie_posts,
+        'POSTS_PER_PAGE' => $posts_per_page,
+        'TOTAL_PAGES' => ceil($total_movie_posts / $posts_per_page),
     );
     
     wp_localize_script('twentyseventeen_child-script', 'REST_API_EXAMPLE', $localize_data);
--- wp-content/themes/twentyseventeen-child/js/script.js
+++ wp-content/themes/twentyseventeen-child/js/script.js
@@ -1,15 +1,31 @@
 (function($) {
   console.log(REST_API_EXAMPLE);
 
-  $.ajax( {
-    url: REST_API_EXAMPLE.SITE_URL + '/wp-json/wp/v2/movie?per_page=20',
-    success: function ( data, status, xhr ) {
-      console.log(data);
-      console.log(xhr);
-      console.log(xhr.getResponseHeader('X-WP-Total'));
-      console.log(xhr.getResponseHeader('X-WP-TotalPages'));
-    }
+//   $.when($.ajax("request1"), $.ajax("request2"), $.ajax("request3"))
+// .then(successCallback, errorHandler);
+
+  function successCallback(xhr) {
+    console.log(xhr);
+  };
+
+  function errorHandler(error) {
+    console.log(error);
+  };
+
+
+  var $ajaxOne = $.ajax( {
+    type: 'GET',
+    dataType: 'json',
+    url: REST_API_EXAMPLE.SITE_URL + '/wp-json/wp/v2/movie?per_page=5',
   } );
+
+  var $ajaxTwo = $.ajax( {
+    type: 'GET',
+    dataType: 'json',
+    url: REST_API_EXAMPLE.SITE_URL + '/wp-json/wp/v2/movie?per_page=5',
+  } );
+
+  $.when([$ajaxOne, $ajaxTwo]).then(successCallback, errorHandler)
 
       
   })( jQuery );
