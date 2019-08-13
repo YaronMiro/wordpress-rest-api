1e7ad20b6e233f01443ab7ba03d7809e78a46b42
yaronm <yaron.miro@gmail.com>
2019-08-13 19:16:23 +0300

    Alter tempalte

--- wp-content/themes/twentyseventeen-child/js/script.js
+++ wp-content/themes/twentyseventeen-child/js/script.js
@@ -1,45 +1,66 @@
 (function($) {
 
-  var posts = [];
-  var xhrCalls = [];
 
-  $( ".entry-content" ).append( "<h5 class=\"loader\">Loading...</h5>" );
-  $( ".entry-content" ).append( "<ul class=\"movies\"></ul>" );
+  $('document').ready(function(){
 
-  for (var page = 1; page <= LOCALIZE.TOTAL_PAGES; page++) {
-    var call = $.ajax({
-      type: 'GET',
-      dataType: 'json',
-      url: LOCALIZE.SITE_URL + `/wp-json/wp/v2/movie?page=${page}&per_page=${LOCALIZE.POSTS_PER_PAGE}`,
-      success: function(data) {
-        posts = posts.concat(data);
-      },
-    });
-    // Add ajax call to the array.
-    xhrCalls.push(call)
+    getMovies();
+
+  })
+
+
+
+
+  function getFilters(){
+
+    var posts = [];
+    var xhrCalls = [];
+    
   }
 
-   // Handle Ajax Success.
-   function successCallback() {
+  function getMovies(){
 
-    // Sort the posts by "ID"
-    posts.sort((a, b) => (a.id > b.id) ? 1 : -1)
-
-    // Log each post title.
-    $( ".entry-content .loader" ).fadeOut();
-    $.each(posts, function(index, post){
-      console.log(post.title.rendered);
-      $(".movies").append(`<li>${post.title.rendered}</li>`);
-
-    })
-  };
+    var posts = [];
+    var xhrCalls = [];
+    $site_main = $( ".site-main" );
+    $site_main.find(".movies-main").append( "<h5 class=\"loader\">Loading...</h5>" );
+    $site_main.find(".movies-main").append( "<ul class=\"movies\"></ul>" );
   
-  // Handle Ajax Error.
-  function errorHandler(error) {
-    console.log('Error: ', error);
-  };
+    for (var page = 1; page <= LOCALIZE.TOTAL_PAGES; page++) {
+      var call = $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: LOCALIZE.SITE_URL + `/wp-json/wp/v2/movie?page=${page}&per_page=${LOCALIZE.POSTS_PER_PAGE}`,
+        success: function(data) {
+          posts = posts.concat(data);
+        },
+      });
+      // Add ajax call to the array.
+      xhrCalls.push(call)
+    }
+  
+     // Handle Ajax Success.
+     function successCallback() {
+  
+      // Sort the posts by "ID"
+      posts.sort((a, b) => (a.id > b.id) ? 1 : -1)
+  
+      // Log each post title.
+      $( ".entry-content .loader" ).fadeOut();
+      $.each(posts, function(index, post){
+        console.log(post.title.rendered);
+        $(".movies").append(`<li>${post.title.rendered}</li>`);
+  
+      })
+    };
+    
+    // Handle Ajax Error.
+    function errorHandler(error) {
+      console.log('Error: ', error);
+    };
+  
+    // Run all ajaxCalls simultaneously.
+    $.when.apply($, xhrCalls).then(successCallback, errorHandler);
 
-  // Run all ajaxCalls simultaneously.
-  $.when.apply($, xhrCalls).then(successCallback, errorHandler);
+  }
 
   })( jQuery );
