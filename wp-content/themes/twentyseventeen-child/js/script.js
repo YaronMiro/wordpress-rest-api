330d0c4bdb5b56063d0921ea83cf5b8d06a778ac
yaronm <yaron.miro@gmail.com>
2019-08-13 11:45:30 +0300

    Get count

--- wp-content/themes/twentyseventeen-child/js/script.js
+++ wp-content/themes/twentyseventeen-child/js/script.js
@@ -2,9 +2,12 @@
   console.log(REST_API_EXAMPLE);
 
   $.ajax( {
-    url: REST_API_EXAMPLE.SITE_URL + '/wp-json/wp/v2/movie?per_page=2',
-    success: function ( data ) {
-      console.log(data)
+    url: REST_API_EXAMPLE.SITE_URL + '/wp-json/wp/v2/movie?per_page=20',
+    success: function ( data, status, xhr ) {
+      console.log(data);
+      console.log(xhr);
+      console.log(xhr.getResponseHeader('X-WP-Total'));
+      console.log(xhr.getResponseHeader('X-WP-TotalPages'));
     }
   } );
 
