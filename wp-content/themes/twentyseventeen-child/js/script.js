658119efe7d7fc1494b2458f050c3de53e51ed08
yaronm <yaron.miro@gmail.com>
2019-08-13 16:17:15 +0300

    clean code

--- wp-content/themes/twentyseventeen-child/js/script.js
+++ wp-content/themes/twentyseventeen-child/js/script.js
@@ -3,12 +3,14 @@
   var posts = [];
   var xhrCalls = [];
 
+  $( ".entry-content" ).append( "<h5 class=\"loader\">Loading...</h5>" );
+  $( ".entry-content" ).append( "<ul class=\"movies\"></ul>" );
+
   for (var page = 1; page <= LOCALIZE.TOTAL_PAGES; page++) {
     var call = $.ajax({
       type: 'GET',
       dataType: 'json',
-      url: LOCALIZE.SITE_URL + `/wp-json/wp/v2/movie?page=${page}&per_page=${LOCALIZE.POSTS_PER_PAGE}&filter[orderby]=id&order=asc`,
-      async: false,
+      url: LOCALIZE.SITE_URL + `/wp-json/wp/v2/movie?page=${page}&per_page=${LOCALIZE.POSTS_PER_PAGE}`,
       success: function(data) {
         posts = posts.concat(data);
       },
@@ -20,9 +22,15 @@
    // Handle Ajax Success.
    function successCallback() {
 
+    // Sort the posts by "ID"
+    posts.sort((a, b) => (a.id > b.id) ? 1 : -1)
+
     // Log each post title.
+    $( ".entry-content .loader" ).fadeOut(100);
     $.each(posts, function(index, post){
       console.log(post.title.rendered);
+      $(".movies").append(`<li>${post.title.rendered}</li>`);
+
     })
   };
   
